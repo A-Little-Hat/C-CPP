b755ff6912f4c230f56f3002e25bebabe4fbf467
A-Little-Hat <soumyasarkar309@gmail.com>
2021-04-09 11:11:39 +0530

    linked list insert beginning-end-desired

---
+++ subham.cpp
@@ -0,0 +1,156 @@
+#include "iostream"
+#include "stdlib.h"
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head;
+
+void beginsert();
+void lastinsert();
+void randominsert();
+void display();
+
+void main()
+{
+    int ch = 0;
+    while (ch != 5)
+    {
+        cout << "\n\n*********Main Menu*********\n";
+        cout << "\nChoose one option from the following list ...\n";
+        cout << "\n===============================================\n";
+        cout << "\n1. Insert at Beginning";
+        cout << "\n2. Insert at End";
+        cout << "\n3. Insert in Desired Location";
+        cout << "\n4. Display";
+        cout << "\n5. Exit";
+        cout << "\n\n Enter Your Choich : ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            beginsert();
+            break;
+        case 2:
+            lastinsert();
+            break;
+        case 3:
+            randominsert();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            cout << "wrong input try again.";
+            ch = 0;
+            break;
+        }
+    }
+}
+void beginsert()
+{
+    struct node *ptr;
+    int item;
+    ptr = (struct node *)malloc(sizeof(struct node *));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW");
+    }
+    else
+    {
+        printf("\nEnter value\n");
+        scanf("%d", &item);
+        ptr->data = item;
+        ptr->next = head;
+        head = ptr;
+        printf("\nNode inserted");
+    }
+}
+void lastinsert()
+{
+    struct node *ptr, *temp;
+    int item;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW");
+    }
+    else
+    {
+        printf("\nEnter value?\n");
+        scanf("%d", &item);
+        ptr->data = item;
+        if (head == NULL)
+        {
+            ptr->next = NULL;
+            head = ptr;
+            printf("\nNode inserted");
+        }
+        else
+        {
+            temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = ptr;
+            ptr->next = NULL;
+            printf("\nNode inserted");
+        }
+    }
+}
+void randominsert()
+{
+    int i, loc, item;
+    struct node *ptr, *temp;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW");
+    }
+    else
+    {
+        printf("\nEnter element value");
+        scanf("%d", &item);
+        ptr->data = item;
+        printf("\nEnter the location after which you want to insert ");
+        scanf("\n%d", &loc);
+        temp = head;
+        for (i = 0; i < loc; i++)
+        {
+            temp = temp->next;
+            if (temp == NULL)
+            {
+                printf("\ncan't insert\n");
+                return;
+            }
+        }
+        ptr->next = temp->next;
+        temp->next = ptr;
+        printf("\nNode inserted");
+    }
+}
+void display()
+{
+    struct node *ptr;
+    ptr = head;
+    if (ptr == NULL)
+    {
+        printf("Nothing to print");
+    }
+    else
+    {
+        printf("\nprinting values . . . . .\n");
+        while (ptr != NULL)
+        {
+            printf("\n%d", ptr->data);
+            ptr = ptr->next;
+        }
+    }
+}
