7566c186525c8857836a91f9924d9967c45c603a
A-Little-Hat <soumyasarkar309@gmail.com>
2022-08-09 21:00:10 +0530

    commit

---
+++ programiz/7.even_odd.cpp
@@ -0,0 +1,15 @@
+#include"iostream"
+using namespace std;
+bool isEven(int x){
+    if(x%2==0)
+        return true;
+    else
+        return false;
+}
+int main(){
+    int a;
+    cout<<"enter two number : ";
+    cin>>a;
+    isEven(a)?cout<<"\n Even":cout<<"\n Odd";
+    return 0;
+}
